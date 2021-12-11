daa040b813406047e04e4f6eb78202b6b7d7c5bf
w2leo <82013108+w2leo@users.noreply.github.com>
2021-12-11 20:24:16 +0300

    Test files add
    Add class Map,
    class TerrainTiles:Map

---
+++ Rave_2DM/Assets/Scripts/TerrainTiles.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainTiles : Map
+{
+    public static int sizeX { get; }
+    public static int sizeY { get; }
+
+
+
+}
